d6994c267d5385792fb8b542afb11bf7ae7eecac
unknown <geoffryskio@gmail.com>
2019-04-24 15:07:34 +1000

    Pyseirn made me do this

---
+++ bin/systems/commands/911.js
@@ -0,0 +1,28 @@
+const {DefaultCommand,CommandProperty,DefaultAlias} = require('../../templates/command');
+const {SimpleMessageResponse} = require('../../messageresponse');
+const Config = require("../../../config");
+const {RichEmbed} = require("discord.js");
+
+class TowerCommand extends DefaultCommand {
+    constructor(mgr) {
+        let properties = new CommandProperty("911");
+        properties.allowDM(true).noArgs().setFixedPermissions(true);
+        properties.forceWhitelist(false);
+        super(mgr, properties);
+    }
+    init() {
+	    let cmdSys = this._manager.getSystem("Commands");
+	    cmdSys.registerAlias(new DefaultAlias("9/11", this))	
+		
+    }
+
+    run(message, args) {
+	    let rich = new RichEmbed();
+	    rich.setTitle("Bush did it");
+        rich.setImage("https://i.ytimg.com/vi/3_MMYI9POc0/maxresdefault.jpg");
+        rich.setFooter("I didn't make this, Pyseirn did.");
+	    return new SimpleMessageResponse(rich);
+    }
+}
+
+module.exports = TowerCommand;
