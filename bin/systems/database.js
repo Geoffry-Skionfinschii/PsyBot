3b631c7ce21a5a55351131328356a01808974d9f
unknown <geoffryskio@gmail.com>
2019-04-26 12:50:20 +1000

    jokes, small error, last last one

--- bin/systems/database.js
+++ bin/systems/database.js
@@ -47,7 +47,7 @@ class DatabaseSystem extends DefaultSystem {
 
     _saveAll() {
         for(let store in this._database) {
-            this._save(this._database[store]);
+            this._save(this._database[store], true);
         }
     }
 
