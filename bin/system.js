523124827cdb0f8706c5e1eaa1c99b501c5ccc22
unknown <geoffryskio@gmail.com>
2019-04-26 14:34:48 +1000

    Added docs to define types

--- bin/system.js
+++ bin/system.js
@@ -5,6 +5,11 @@ class DefaultSystem {
     //Init function is where the system can set up its own values.
     //It will be passed the TimingManager class where it can add new timers
 
+    /**
+     * @typedef {import('./manager')} Manager
+     * @param {Manager} mgr 
+     * @param {string} systemName 
+     */
     constructor(mgr, systemName) {
         this._manager = mgr;
         this._identity = systemName;
